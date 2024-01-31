a74e42620c16a62e46d6719272a016dc7c3da2bf
Custromix <75646944+Custromix@users.noreply.github.com>
2024-02-01 00:27:03 +0100

    uodate bot

--- settings.py
+++ settings.py
@@ -1,4 +1,24 @@
 import os
+from logging.config import dictConfig
+from dotenv import load_dotenv
 
+load_dotenv()
+DISCORD_API_SECRET = os.getenv('DISCORD_API_TOKEN')
 
-DISCORD_API_SECRET = os.getenv('DISCORD_API_SECRET')
+LOGGING_CONFIG = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters":{
+        "verbose":{
+            "format"
+        }
+    },
+    "handlers":{
+
+    },
+    "loggers":{
+
+    }
+}
+
+dictConfig(LOGGING_CONFIG)
